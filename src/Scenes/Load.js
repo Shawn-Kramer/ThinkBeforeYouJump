07bd01218981e0fd8dc3525ffbac10ef31e8ba2a
Shawn-Kramer <shawn.k.kramer@gmail.com>
2025-05-27 00:19:23 -0700

    Add files via upload

---
+++ src/Scenes/Load.js
@@ -0,0 +1,65 @@
+class Load extends Phaser.Scene {
+    constructor() {
+        super("loadScene");
+    }
+
+    preload() {
+        this.load.setPath("./assets/");
+
+        // Load characters spritesheet
+        this.load.atlas("platformer_characters", "tilemap-characters_packed.png", "tilemap-characters-packed.json");
+
+        // Load tilemap information
+        this.load.image("tilemap_tiles", "tilemap_packed.png");
+        this.load.tilemapTiledJSON("level1", "ThinkBeforeYouJumpLevel1.tmj");
+
+        // Load the background tiles
+        this.load.image("background_tiles", "tilemap-backgrounds_packed.png");
+
+        // Load the tilemap as a spritesheet
+        this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
+            frameWidth: 18,
+            frameHeight: 18
+        });
+
+        this.load.multiatlas("kenny-particles", "kenny-particles.json");
+
+        //document.getElementById('description').innerHTML = '<h2>Monster.js<br>S - smile // F - show fangs<br>A - move left // D - move right</h2>'
+    }
+
+    create() {
+        this.anims.create({
+            key: 'walk',
+            frames: this.anims.generateFrameNames('platformer_characters', {
+                prefix: "tile_",
+                start: 0,
+                end: 1,
+                suffix: ".png",
+                zeroPad: 4
+            }),
+            frameRate: 15,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'idle',
+            defaultTextureKey: "platformer_characters",
+            frames: [
+                { frame: "tile_0000.png" }
+            ],
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'jump',
+            defaultTextureKey: "platformer_characters",
+            frames: [
+                { frame: "tile_0001.png" }
+            ],
+        });
+
+         this.scene.start('thinkBeforeYouJumpScene');
+    }
+    update() {
+    }
+}
